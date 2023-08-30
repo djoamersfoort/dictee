f2b38b9543913f85fa72a55672ac120c37fc7527
ILoveAndLikePizza <nathankoning@gmail.com>
2023-08-30 21:33:27 +0200

    Reglement geüpdatet

--- reglement/index.php
+++ reglement/index.php
@@ -21,8 +21,7 @@ setcookie("REGLEMENT", "1", time() + 86400 * 180, "/");
 <div id="dictee">
 <h3>1. Regels dictee</h3>
 <ol>
-<li><b>Alle woorden en zinnen beginnen met een hoofdletter! Let daarop!</b></li>
-<li>Alle zinnen eindigen met een punt, uitroepteken of vraagteken. De woorden dus niet.</li>
+<li>Gebruik van hoofdletters en plaatsing van trema's wordt naast de spelling ook beoordeeld in het dictee.</li>
 <li>Getallen dienen conform de gebruikelijke richtlijnen uitgeschreven te worden. <a href="https://www.onzetaal.nl/taalloket/getallen-in-letters-of-cijfers" target="_blank">Meer informatie</a></li>
 </ol>
 <h3>2. Regels kandidaat</h3>
@@ -42,22 +41,25 @@ setcookie("REGLEMENT", "1", time() + 86400 * 180, "/");
 </ol>
 <h3>3. Regels examinator</h3>
 <ol>
-<li>Spreek elk woord 3 keer duidelijk uit met een interval van ten minste 5 seconden.</li>
-<li>Spreek elke zin 3 keer volledig uit met een interval van ten minste 10 seconden en herhaal elke zin 1 keer stap voor stap.</li>
-<li>Het is niet toegestaan om het woord/de zin vaker dan bovengenoemde aantallen te zeggen.</li>
-<li>Het is toegestaan om een gedeelte van een zin te herhalen.</li>
-<li>Geef kandidaten de tijd; ga pas verder wanneer zij allemaal gereed zijn.</li>
-<li>Het is niet toegestaan om uitleg te geven over de woorden en zinnen.</li>
-<li>Zorg ervoor dat de kandidaten en omstanders zichzelf geen inzage verschaffen tot de antwoorden.</li>
+<li>De examinator dient de tekst van het dictee in een rustig tempo te doorlopen.</li>
+<li>De examinator dient de tekst op verstaanbare wijze aan de kandidaten over te brengen.</li>
+<li>De examinator dient na het uitspreken van een woord dat de kandidaten moeten spellen een kleine pauze te nemen, alvorens verder te gaan met voorlezen.</li>
+<li>De examinator dient een woord slechts eenmalig uit te spreken. Indien een kandidaat om herhaling vraagt, dient dit opgevat te worden als een overtreding van artikel 2.12 van dit reglement.</li>
+<li>Het is de examinator niet toegestaan om definities te geven van de woorden.</li>
+<li>De examinator is er verantwoordelijk voor dat kandidaten en omstanders zichzelf geen inzage verschaffen tot de antwoorden.</li>
 </ol>
 <h3>4. Zak-/slaagregeling</h3>
-<p>Een kandidaat is geslaagd als deze ten minste de helft van de woorden en zinnen goed gespeld en geformuleerd heeft.
-<br>Als een kandidaat is geslaagd, ontvangt deze het DJO Dictee-certificaat van zijn/haar examinator.
-<br>Indien het dictee in groepsverband is afgenomen, kan de examinator aan de kandidaat met de meeste goede antwoorden
-een extra prijs uitreiken, ongeacht of deze geslaagd of gezakt is.</p>
+<ol>
+<li>Een kandidaat is geslaagd als deze ten minste de helft van de woorden goed gespeld en geformuleerd heeft.</li>
+<li>Als een kandidaat is geslaagd, ontvangt deze het DJO Dictee-certificaat van zijn/haar examinator.</li>
+<li>Indien het dictee in groepsverband is afgenomen, kan de examinator aan de kandidaat met de meeste goede antwoorden
+een extra prijs uitreiken, ongeacht of deze geslaagd of gezakt is.</li>
+</ol>
 <h3>5. Overtredingen</h3>
-<p>Bij de eerste overtreding van het reglement gaat er een waarschuwing uit naar de overtreder.
-<br>Bij de tweede overtreding dient de overtreder te worden verwijderd uit het dictee.</p>
+<ol>
+<li>Bij de eerste overtreding van het reglement gaat er een waarschuwing uit naar de overtreder.</li>
+<li>Bij de tweede overtreding dient de overtreder te worden verwijderd uit het dictee.</li>
+</ol>
 </div>
 <table style="width:50%"><tr><td class="maincard">
 <h2>Uitgelezen?</h2>
