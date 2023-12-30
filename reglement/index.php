5571007ef658108e4cb5643e2b79b1af8e696f9d
ChrisDo28 <108572774+ChrisDo28@users.noreply.github.com>
2023-12-30 11:30:28 +0100

    Update het regelement

--- reglement/index.php
+++ reglement/index.php
@@ -17,51 +17,89 @@ setcookie("REGLEMENT", "1", time() + 86400 * 5, "/");
 <div id="mainhead">
 <h1>Reglement</h1>
 <h2>Lees het reglement voorafgaand aan het dictee grondig door.</h2>
-</div>
-<div id="dictee">
-<h3>1. Regels dictee</h3>
-<ol>
-<li>Gebruik van hoofdletters en plaatsing van trema's wordt naast de spelling ook beoordeeld in het dictee.</li>
-<li>Getallen dienen conform de gebruikelijke richtlijnen uitgeschreven te worden. <a href="https://www.onzetaal.nl/taalloket/getallen-in-letters-of-cijfers" target="_blank">Meer informatie</a></li>
-</ol>
-<h3>2. Regels kandidaat</h3>
-<ol>
-<li>Het is verplicht om het dictee af te leggen met een door Kwabbel, Inc. erkende examinator.</li>
-<li>Het is verplicht om het dictee af te leggen op een door N&amp;M Games goedgekeurd apparaat.</li>
-<li>Het is verplicht om eerlijk je voor- én achternaam op te geven.</li>
-<li>Het is verplicht om de browser waarin het dictee wordt afgenomen in volledig scherm te zetten.</li>
-<li>Het is niet toegestaan om af te kijken of te spieken bij andere deelnemers.</li>
-<li>Het is niet toegestaan om spellingscontrole te gebruiken.</li>
-<li>Het is niet toegestaan om hulp te vragen van een ander, ongeacht of deze deelneemt aan het dictee of niet.</li>
-<li>Het is niet toegestaan om antwoorden aan andere DJO'ers kenbaar te maken.</li>
-<li>Het is niet toegestaan om de browserconsole, de inspecteur of iets van dergelijke aard te gebruiken tijdens het dictee.</li>
-<li>Het is niet toegestaan om onnodig oogcontact te maken met surveillanten, examinatoren en voorbijgangers.</li>
-<li>Het is niet toegestaan om resultaten van het dictee op enigerlei wijze te kopiëren.</li>
-<li>Het is niet toegestaan om eenzelfde dictee meer dan één keer af te leggen.</li>
-<li>Het is niet toegestaan om te praten met andere deelnemers, surveillanten of examinatoren.</li>
-</ol>
-<h3>3. Regels examinator</h3>
-<ol>
-<li>De examinator dient de tekst van het dictee in een rustig tempo te doorlopen.</li>
-<li>De examinator dient de tekst op verstaanbare wijze aan de kandidaten over te brengen.</li>
-<li>De examinator dient na het uitspreken van een woord dat de kandidaten moeten spellen een kleine pauze te nemen, alvorens verder te gaan met voorlezen.</li>
-<li>De examinator dient een woord slechts eenmalig uit te spreken. Indien een kandidaat om herhaling vraagt, dient dit opgevat te worden als een overtreding van artikel 2.12 van dit reglement.</li>
-<li>Het is de examinator niet toegestaan om definities te geven van de woorden.</li>
-<li>De examinator is er verantwoordelijk voor dat kandidaten en omstanders zichzelf geen inzage verschaffen tot de antwoorden.</li>
-</ol>
-<h3>4. Zak-/slaagregeling</h3>
-<ol>
-<li>Een kandidaat is geslaagd als deze ten minste de helft van de woorden goed gespeld en geformuleerd heeft.</li>
-<li>Als een kandidaat is geslaagd, ontvangt deze het DJO Dictee-certificaat van zijn/haar examinator.</li>
-<li>Indien het dictee in groepsverband is afgenomen, kan de examinator aan de kandidaat met de meeste goede antwoorden
-een extra prijs uitreiken, ongeacht of deze geslaagd of gezakt is.</li>
-</ol>
-<h3>5. Overtredingen</h3>
-<ol>
-<li>Bij de eerste overtreding van het reglement gaat er een waarschuwing uit naar de overtreder.</li>
-<li>Bij de tweede overtreding dient de overtreder te worden verwijderd uit het dictee.</li>
-</ol>
-</div>
+<div id="Dictee">
+        <h3>I. Algemene Spelregels</h3>
+        <ul>
+            <li>Spelling en Notatie
+                <ul>
+                    <li>Correct gebruik van hoofdletters, trema's en interpunctie wordt naast de spelling beoordeeld.</li>
+                    <li>Getallen dienen conform de gebruikelijke richtlijnen te worden uitgeschreven.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>II. Voorwaarden voor Deelnemers</h3>
+        <ul>
+            <li>Deelnamevereisten
+                <ul>
+                    <li>Het dictee moet worden afgenomen onder toezicht van een erkende examinator.</li>
+                    <li>Gebruik van een door N&amp;M Games goedgekeurd apparaat is verplicht.</li>
+                    <li>De deelnemer moet eerlijk zijn/haar volledige naam verstrekken.</li>
+                    <li>Het dictee moet in volledig schermmodus worden afgelegd in de browser.</li>
+                </ul>
+            </li>
+            <li>Verboden Handelingen
+                <ul>
+                    <li>Geen spieken of afkijken bij andere deelnemers.</li>
+                    <li>Geen gebruik van spellingscontrole.</li>
+                    <li>Geen hulp vragen aan anderen, ongeacht deelname aan het dictee.</li>
+                    <li>Geen bekendmaking van antwoorden aan andere deelnemers.</li>
+                    <li>Geen gebruik van browserfuncties zoals de console of inspectietools.</li>
+                    <li>Vermijd onnodig oogcontact met toezichthouders.</li>
+                    <li>Geen duplicatie of kopiëren van dictee-uitslagen.</li>
+                    <li>Slechts één keer deelnemen aan hetzelfde dictee.</li>
+                    <li>Geen communicatie met andere deelnemers, toezichthouders of examinatoren.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>III. Taken van de Examinator</h3>
+        <ul>
+            <li>Leesproces
+                <ul>
+                    <li>De examinator moet de tekst van het dictee in een rustig tempo voorlezen.</li>
+                    <li>Duidelijke en begrijpelijke uitspraak van de woorden naar de deelnemers toe.</li>
+                    <li>Een korte pauze nemen na het spellen van een woord voordat verder wordt gegaan met voorlezen.</li>
+                </ul>
+            </li>
+            <li>Herhaling van Woorden
+                <ul>
+                    <li>Een woord wordt slechts één keer uitgesproken.</li>
+                </ul>
+            </li>
+            <li>Verboden Acties
+                <ul>
+                    <li>Geen verstrekking van definities van woorden aan de deelnemers.</li>
+                    <li>Voorkomen dat deelnemers of omstanders toegang krijgen tot de antwoorden.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>IV. Beoordeling en Certificering</h3>
+        <ul>
+            <li>Slaagcriteria
+                <ul>
+                    <li>Een kandidaat is geslaagd als deze ten minste de helft van de woorden goed gespeld en geformuleerd heeft.</li>
+                    <li>Als een kandidaat is geslaagd, ontvangt deze het DJO Dictee-certificaat van zijn/haar examinator.</li>
+                </ul>
+            </li>
+            <li>Erkenning van Uitblinkers
+                <ul>
+                    <li>De examinator kan een extra prijs toekennen aan de deelnemer met de meeste correcte antwoorden in een groepsdictee.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>V. Sancties bij Overtredingen</h3>
+        <ul>
+            <li>Waarschuwingen en Verwijdering
+                <ul>
+                    <li>Eerste overtreding leidt tot een waarschuwing aan de overtreder.</li>
+                    <li>Bij herhaalde overtreding wordt de overtreder uit het dictee verwijderd.</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 <table style="width:50%"><tr><td class="maincard">
 <h2>Uitgelezen?</h2>
 <p>Dan kun je nu meedoen aan het DJO Dictee!</p>
