69d213101e77fd00f95d77ebd5be6126981fdbd1
SweatyCircle439 <sweatycircle439@sweatycircle439.com>
2026-02-21 10:31:32 +0100

    Hide wait for available step in instructions when already available

--- static/js/index.js
+++ static/js/index.js
@@ -157,6 +157,12 @@ socket.on("dictee-version", v => document.getElementById("version").textContent 
 socket.on("dictee-state", (state, waiting, max) => {
     const full = (waiting === max);
 
+    if (state === "open" && !full) {
+        document.getElementById("waitAvailable").style.display = "none";
+    }else {
+        document.getElementById("waitAvailable").style.display = "";
+    }
+
     const buttonText = (state === "closed") ? "Niet beschikbaar" :
       (state === "open" && full) ? "Vol" :
       (state === "open") ? "Beschikbaar" : "Bezig";
